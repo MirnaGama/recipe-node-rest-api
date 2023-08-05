ff50c77617ab37701b3b07263452ac30b5dd232a
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-08-05 18:58:58 -0300

    TSK0 Sync all db tables in the routes index file

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,5 +1,15 @@
 var router = require('express').Router();
 
+// Sync all tables
+(async () => {
+    const {sequelizeConnect} = require('../../config/db.js');
+    try {
+        const result = await sequelizeConnect.sync();
+    } catch (error) {
+        console.log(error);
+    }
+})();
+
 router.use((req, res, next) => {
     if (req.cookies.SessionCookie && !req.session.user) {
         res.clearCookie('SessionCookie');
