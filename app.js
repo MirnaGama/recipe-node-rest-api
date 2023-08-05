08d0d591933f7a3525d5c06a09235501bc20ca15
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-08-05 18:37:34 -0300

    TSK0 Initialize project and create initial endpoint

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+  });
+
+  app.get("/status", (request, response) => {
+    const status = {
+       "Status”": "Running"
+    };
+    
+    response.send(status);
+ });
