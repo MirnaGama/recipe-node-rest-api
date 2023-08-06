c7c613cc1f7c1e79dc96f5ae8ba50e8a440f37b6
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-08-06 19:36:16 -0300

    TSK1 Set session and cookie header in the request

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
 app.use(express.json());
+app.use(cookieParser());	// For parsing Cookie header and populate req.cookies with an object keyed by the cookie names
+
+app.use(session({
+   name: 'SessionCookie',
+   secret: 'Shsh!Secret!',
+   resave: false,
+   saveUninitialized: false,
+   cookie: { maxAge: 600000, httpOnly: false } // Secure = True to use Secure HTTPS
+}));
 
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
