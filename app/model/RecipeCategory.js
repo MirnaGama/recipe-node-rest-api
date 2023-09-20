6c5f5207f006e2353f0b8936c99eeacb43ac64a2
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-09-19 21:30:18 -0300

    TSK2 Add initial data for recipe categories

--- app/model/RecipeCategory.js
+++ app/model/RecipeCategory.js
@@ -15,4 +15,48 @@ const RecipeCategory = sequelizeConnect.define('recipe_category', {
     }
 })
 
+// The method findOrCreate will create an entry in the table unless it can find one (i.e.: insert if does not exist)
+
+RecipeCategory.sync().then(() => {
+    RecipeCategory.findOrCreate({
+        where: { label: 'Breakfast' }
+      });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Lunch' }
+      });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Appetizer' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Salad' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Main-course' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Side-dish' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Baked-goods' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Dessert' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Snack' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Soup' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Holiday' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Vegetarian' }
+    });
+    RecipeCategory.findOrCreate({
+        where: { label: 'Vegan' }
+    });
+  });
+
 module.exports = RecipeCategory;
